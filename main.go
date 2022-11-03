e725de91712795ccdb7be954d6b37a49b6a16d9e
kalebslkinch <kalebslkinch@gmail.com>
2022-11-03 16:39:13 +0000

    add podcast search menu

--- main.go
+++ main.go
@@ -7,23 +7,29 @@ import (
 	"log"
 	"net/http"
 	"io/ioutil"
-
 )
 
-
-func getPopularPodcasts() {
-	handler("https://podcast-index-platform.vercel.app/api/trpc/podIndex.homePage?batch=1&input=%7B%220%22%3A%7B%22json%22%3Anull%2C%22meta%22%3A%7B%22values%22%3A%5B%22undefined%22%5D%7D%7D%7D")
+func getPopularPodcasts(terminal *tview.Application) {
+	handler("https://podcast-index-platform.vercel.app/api/trpc/podIndex.homePage?batch=1&input=%7B%220%22%3A%7B%22json%22%3Anull%2C%22meta%22%3A%7B%22values%22%3A%5B%22undefined%22%5D%7D%7D%7D","API", terminal)
 }
 
-func getHomepageData() {
-	handler("https://podcast-index-platform.vercel.app/api/cachedAPI/homePage")
+func getHomepageData(terminal *tview.Application) {
+	handler("https://podcast-index-platform.vercel.app/api/cachedAPI/homePage","API", terminal)
 }
 
-func getTrendingPageData() {
-	handler("https://podcast-index-platform.vercel.app/api/cachedAPI/trendingPage")
+func getTrendingPageData(terminal *tview.Application) {
+	handler("https://podcast-index-platform.vercel.app/api/cachedAPI/trendingPage","API", terminal)
 }
-func handler(url string) {
 
+func getPodcastById(id string, terminal *tview.Application) {
+	handler("https://podcast-index-platform.vercel.app/api/searchBy/searchById/?id="+ id, "SEARCH",terminal)
+}
+
+func getTrendingPodcasts(max string, terminal *tview.Application) {
+	handler("https://podcast-index-platform.vercel.app/api/searchBy/searchByTrending?max=" + max ,"SEARCH",terminal)
+}
+
+func handler(url string, handlerType string, terminal *tview.Application) {
 req, err := http.NewRequest("GET",  url, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -44,22 +50,27 @@ req, err := http.NewRequest("GET",  url, nil)
 
 	fmt.Println("Press any key to return to menu")
 	fmt.Scanln()
-	showAPIMenu(tview.NewApplication())
-	
+
+	if handlerType == "API" {		
+		showAPIMenu(terminal)
+	}
+
+	if handlerType == "SEARCH" {
+		showSearchMenu(terminal)
+	}
 	
 }
-func mainmenu() tview.Primitive {
+
+func mainMenu() tview.Primitive {
 	list := tview.NewList().
 		AddItem("Check API Endpoints", "", '1', nil).
-		AddItem("Menu Option 2", "", '2', nil).
+		AddItem("Search", "", '2', nil).
 		AddItem("Menu Option 3", "", '3', nil).
 		AddItem("Quit", "", '4', nil)
-
 	return list
-	
 }
 
-func apimenu() tview.Primitive {
+func apiMenu() tview.Primitive {
 	list := tview.NewList().
 		AddItem("View Popular Podcasts Data", "", '1', nil).
 		AddItem("View Home Page Cached Data", "", '2', nil).
@@ -69,74 +80,126 @@ func apimenu() tview.Primitive {
 	
 }
 
+func mainSearchMenu() tview.Primitive {
+	list := tview.NewList().
+		AddItem("Podcast", "", '1', nil).
+		AddItem("Episode", "", '2', nil).
+		AddItem("Return to Menu", "", '3', nil)
+	return list
+}
 
+func searchPodcastMenu() tview.Primitive {
+	list := tview.NewList().
+		AddItem("Search by Feed ID", "", '1', nil).
+		AddItem("Search Trending Podcasts", "", '2', nil).
+		AddItem("Return to Menu", "", '3', nil)
+	return list
+}
+
+func searchEpisodeMenu() tview.Primitive {
+	list := tview.NewList().
+		AddItem("Search by ID", "", '1', nil).
+		AddItem("Search Trending Podcasts", "", '2', nil).
+		AddItem("Return to Menu", "", '3', nil)
+	return list
+}
+
+func showPodcastSearchMenu(terminal *tview.Application) {
+	terminal.SetRoot(searchPodcastMenu(), true)
+	terminal.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Rune() {
+			case '1': 
+				fmt.Println("Enter Feed ID")
+				var id string
+				fmt.Scanln(&id)
+				getPodcastById(id,terminal)
+
+			case '2':
+				fmt.Println("Enter Max")
+				var max string
+				fmt.Scanln(&max)
+				getTrendingPodcasts(max,terminal)
+
+			case '3':
+				terminal.Stop()
+				main()
+		}
+
+		return event
+	})
+}
+
+func showSearchMenu(terminal *tview.Application) {
+	terminal.SetRoot(mainSearchMenu(), true)
+	terminal.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		
+		if event.Key() == tcell.KeyRune {
+
+		switch event.Rune() {
+		case '1':
+			showPodcastSearchMenu(terminal)
+		case '2':
+			searchEpisodeMenu()
+		case '3':
+			terminal.Stop()
+				main()
+		}
+	}
+
+		return event
+	})
+}
+			
 func showAPIMenu(terminal *tview.Application) {
 
-	terminal.SetRoot(apimenu(), true)
+	terminal.SetRoot(apiMenu(), true)
 	terminal.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Key() == tcell.KeyEscape {
-			terminal.Stop()
-		}
+	
 		if event.Key() == tcell.KeyRune {
+			
 			if event.Rune() == '1' {
-			terminal.Stop()
-			getPopularPodcasts()
-
-		}
+				getPopularPodcasts(terminal)
+			}	
 			if event.Rune() == '2' {
-			terminal.Stop()
-			getHomepageData()
-
+				getHomepageData(terminal)
 			}
 			if event.Rune() == '3' {
-			terminal.Stop()
-			getTrendingPageData()
-
+				getTrendingPageData(terminal)	
 			}
-			terminal.Stop()
 			if event.Rune() == '4'  {
 				terminal.Stop()
 				main()
 			}
 		}
 		
-
 		return event
 	})
 }
+
 func main() {
 
 	terminal := tview.NewApplication()
 
-	terminal.SetRoot(mainmenu(), true)
+	terminal.SetRoot(mainMenu(), true)
 	terminal.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyEscape {
 			terminal.Stop()
 		}
 		if event.Key() == tcell.KeyRune {
 			if event.Rune() == '1' {
-
-			showAPIMenu(terminal)
-
+				showAPIMenu(terminal)
 		}
 			if event.Rune() == '2' {
-			terminal.Stop()
-			getHomepageData()
-
+				showSearchMenu(terminal)
 			}
 			if event.Rune() == '3' {
-			terminal.Stop()
-			getTrendingPageData()
-
+				getTrendingPageData(terminal)  
 			}
 			
 		}
 		
-
 		return event
 	})
-
-
 
 	if err := terminal.Run(); err != nil {
 		panic(err)
